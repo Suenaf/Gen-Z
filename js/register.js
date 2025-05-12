68091e8c7403fbfe6ea94f6ec0c377a53c95ba9b
springfocused <rabih.hajhassan@stud.fhgr.ch>
2025-05-12 11:24:49 +0200

    first push

---
+++ js/register.js
@@ -0,0 +1,17 @@
+console.log("register.js loaded");  
+
+document
+.getElementById("registerForm")
+.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    let email = document.querySelector("#email").value;
+    console.log(email);
+
+    let username = document.querySelector("#username").value;
+    console.log(username);
+
+    let password = document.querySelector("#password").value;
+    console.log(password);
+});
+
