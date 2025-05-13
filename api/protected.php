24c860f077d24e3abecf4ca19dded941031f6a41
springfocused <rabih.hajhassan@stud.fhgr.ch>
2025-05-13 13:20:42 +0200

    geschütztes einloggen aufgebaut

---
+++ api/protected.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    
+    echo json_encode([
+        "status" => "success",
+        "user_id" => $_SESSION['user_id'],
+        "email" => $_SESSION['email'],
+        "username" => $_SESSION['username'],
+        ]);
+
+} else {
+
+    echo json_encode([
+        "status" => "error",
+    ]);
+    
+    session_destroy();
+}
